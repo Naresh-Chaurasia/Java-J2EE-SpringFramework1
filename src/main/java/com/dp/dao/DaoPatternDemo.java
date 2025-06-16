53bf1b11196f9cc98a27e66dc4621bcf3503512d
Naresh Chaurasia <johndoe@example.com>
2025-06-16 10:35:33 +0530

    Macbook Pro, Checkin Timestamp::Mon Jun 16 10:35:33 IST 2025

--- src/main/java/com/dp/dao/DaoPatternDemo.java
+++ src/main/java/com/dp/dao/DaoPatternDemo.java
@@ -1,5 +1,31 @@
 package com.dp.dao;
 
 public class DaoPatternDemo {
-    
+
+    public static void main(String[] args) {
+        UserDao userDao = new UserDaoImpl();
+
+        // Add users
+        userDao.addUser(new User(1, "Alice"));
+        userDao.addUser(new User(2, "Bob"));
+
+        // Get all users
+        for (User user : userDao.getAllUsers()) {
+            System.out.println(user.getId() + ": " + user.getName());
+        }
+
+        // Update user
+        User user = userDao.getUser(1);
+        user.setName("Alice Smith");
+        userDao.updateUser(user);
+
+        // Delete user
+        userDao.deleteUser(2);
+
+        // Get all users after update and delete
+        for (User u : userDao.getAllUsers()) {
+            System.out.println(u.getId() + ": " + u.getName());
+        }
+    }
+
 }
