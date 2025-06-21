c8ebf52cf83a837b51186ea6beb34643c5a4d69f
Naresh Chaurasia <johndoe@example.com>
2025-06-21 10:55:44 +0530

    Macbook Pro, Checkin Timestamp::Sat Jun 21 10:55:44 IST 2025

--- src/main/java/com/springaop/AopConfig.java
+++ src/main/java/com/springaop/AopConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+// "Hey Spring, please look for any classes marked with @Aspect and apply their advice (like logging, security checks, etc.) automatically when the matching methods run."
 @Configuration
 @ComponentScan(basePackages = "com.springaop")
 @EnableAspectJAutoProxy
